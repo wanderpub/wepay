2e633ec208e35b70de980901f4e948e95d1743b8
wanders <13834563@qq.com>
2021-08-25 13:48:57 +0800

    sdf

---
+++ WePayPartner/Basic/Cert.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WePayPartner\Basic;
+
+use WePay\Exceptions\InvalidResponseException;
+use WePayPartner\Basic\BasicWePay;
+use WePay\Basic\DecryptAes;
+
+/**
+ * 平台证书管理
+ * Class Cert
+ * @package WePay
+ */
+class Cert extends BasicWePay
+{
+
+    /**
+     * 商户平台下载证书
+     * @throws InvalidResponseException
+     */
+    public function download()
+    {
+        try {
+            $aes = new DecryptAes($this->config['mch_v3_key']);
+            $result = $this->doRequest('GET', '/v3/certificates');
+            foreach ($result['data'] as $vo) {
+                $this->tmpFile($vo['serial_no'], $aes->decryptToString(
+                    $vo['encrypt_certificate']['associated_data'],
+                    $vo['encrypt_certificate']['nonce'],
+                    $vo['encrypt_certificate']['ciphertext']
+                ));
+            }
+        } catch (\Exception $exception) {
+            throw new InvalidResponseException($exception->getMessage(), $exception->getCode());
+        }
+    }
+}
